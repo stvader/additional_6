000e5ef9cffa00a0ef2ab02337831aa914d461e9
Vadim St <vadim_st_13@mail.ru>
2017-09-15 16:06:19 +0300

    find trouble

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function zeros(expression) {
-  // your solution
+	let factorialArray = expression.split('*');
+	let zeroCount = 0;
+
+	factorialArray.forEach(function(item) {
+		let itemNum = parseInt(item);
+		let step;
+		(item.slice(-2) === '!!') ? step = 1 : step = 2;
+		
+		for (let i=itemNum; i>=1; i-step) {
+			if (i%25 === 0) zeroCount++;
+			if (i%5 === 0) zeroCount++;
+		}//trouble is in this circle 
+	});
+
+	return zeroCount;
 }
